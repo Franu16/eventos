32554bdf86e9e5d0ff46a19fbdd0dd2eda624cae
Franu16 <torresfrancesca194@gmail.com>
2025-08-21 16:30:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//creamos la funcion para cerrar sesión
+function cerrarSesión (){
+    alert("cerraste sesión,gracias por la visita")
+}
+function salirPagina(){
+ location.href="https://www.google.com/";
+}
